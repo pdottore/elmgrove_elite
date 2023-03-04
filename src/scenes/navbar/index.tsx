48d068cfdc07eff37c824fa433a609c7f64222da
Patrick Dottore (Student) <pdottore@sundevils.asu.edu>
2023-03-04 01:44:00 -0500

    Deploy website

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -53,7 +53,7 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
                                 </div>
                                 <div className={`${flexBetween} gap-8`}>
                                     <p>Sign In</p>
-                                    <ActionButton setSelectedPage={selectedPage}>Become a Member</ActionButton>
+                                    <ActionButton setSelectedPage={setSelectedPage}>Become a Member</ActionButton>
                                 </div>
                             </div>) 
                             : (
